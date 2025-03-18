fca32b50b6cd03c34a8fbe52371fcab0c5e376b3
Sokolov Oleg <oleg-sokol.sokolov2016@yandex.ru>
2025-03-18 14:21:07 +0500

    header

--- checkpost/cp/models.py
+++ checkpost/cp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    text = models.CharField(max_length=255, null=True)
+    img = models.CharField(max_length=255, null= True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_com = models.CharField(max_length=255)
+    number_post = models.ForeignKey(Post, on_delete=models.CASCADE)
